6d92d70dea21021f1fc9c7e1c9820bb9e9692fa1
QuentinNeuro <quentin.chevy@gmail.com>
2018-10-25 14:39:21 -0400

    move evernote email config and sending to w/in analysis launcher

--- Analysis_Photometry_Launcher_PostRec.m
+++ Analysis_Photometry_Launcher_PostRec.m
@@ -66,6 +66,29 @@ tic
 disp(['Saving Figure to ' SummaryPlotPath])
 saveas(gcf,SummaryPlotPath);
 toc
+
+% email configuration 
+setpref('Internet','E_mail','kepecslab.cshl@gmail.com')
+setpref('Internet','SMTP_Server','smtp.gmail.com')
+setpref('Internet','SMTP_Username','kepecslab.cshl@gmail.com')
+setpref('Internet','SMTP_Password','D3cision')
+props = java.lang.System.getProperties;
+props.setProperty('mail.smtp.auth','true');
+props.setProperty('mail.smtp.socketFactory.class', 'javax.net.ssl.SSLSocketFactory');
+props.setProperty('mail.smtp.socketFactory.port','465');
+
+ENemail = 'egibson214.3941e4d@m.evernote.com';
+
+% note content
+text2add = strcat(' ----------------------------------------------------',...
+    Analysis.Parameters.Name, [' in ' Analysis.Parameters.Rig],...
+    ' ----------------------------------------------------');
+noteTitle = strcat(Analysis.Parameters.Animal, ' @Sensors mice +');
+tic
+disp('Sending summary plot to Evernote notebook');
+sendmail(ENemail, noteTitle, text2add, {SummaryPlotPath});
+toc
+
 %% Pop-up water supplement reminder
 water2Supplement = 800 - Water;
 msgbox({['Give ' Analysis.Parameters.Animal ' ' num2str(water2Supplement) 'uL water']}, 'Water reminder')
