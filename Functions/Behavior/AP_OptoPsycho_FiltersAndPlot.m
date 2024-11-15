45f623ce2204cecb4faa7dcb54e4dd1f2c42abb9
Quentin <quentin.chevy@gmail.com>
2024-11-15 17:45:41 -0500

    change RT calculation

--- Functions/Behavior/AP_OptoPsycho_FiltersAndPlot.m
+++ Functions/Behavior/AP_OptoPsycho_FiltersAndPlot.m
@@ -18,7 +18,7 @@ for tt=1:nbOfTrialTypes;
     trialCount_Go=Analysis.(thistype_Go).nTrials;
     
     successRate(tt)=trialCount_Go/trialCount_All;
-    thisRT=Analysis.(thistype_Go).Time.Outcome(:,1)-Analysis.(thistype_Go).Time.Cue(:,1);
+    thisRT=diff(Analysis.(thistype_Go).Time.Cue,[],2);
     RT{tt}=thisRT;
     RT_AVG(tt)=mean(thisRT);
 end
