7fc0b151b87432c8e6c1e940cf8a279c6140fe5e
Quentin <quentin.chevy@gmail.com>
2025-05-22 14:41:32 -0400

    gonogo performance
    cosmetic

--- Functions/Behavior/AB_GoNogo_Performance.m
+++ Functions/Behavior/AB_GoNogo_Performance.m
@@ -42,15 +42,16 @@ plot(-1,0,['k' stateID_shape(1)])
 plot(-1,0,['k' stateID_shape(2)])
 
 subplot(2,1,2); hold on
-x=tSequence(fGo);
+xp=tSequence(fGo);
 y=movmedian(licks(fGo),5);
-plot(x,y,'-k')
-x=tSequence(fNoGo);
+plot(xp,y,'-k')
+xp=tSequence(fNoGo);
 y=movmedian(licks(fNoGo),5);
-plot(x,y,'--k')
+plot(xp,y,'--k')
 
 %% Run through blocs
 for b=1:length(blocID)
+    try
     thisBloc=bSequence==b;
     thisTrials=tSequence(thisBloc);
     thisTrialType=ttSequence(thisBloc);
@@ -70,43 +71,59 @@ for b=1:length(blocID)
     hitRate(b)=sum(hit)/sum(thisfGo);
     FARate(b)=sum(FA)/sum(thisfNoGo);
     dprime(b)=norminv(hitRate(b))-norminv(FARate(b));
-    
+    lickRate=[mean(licks(thisTrials(thisfGo))),mean(licks(thisTrials(thisfNoGo)))];
 % Figure
     subplot(2,1,1);
-    x=thisTrials(hit);
-    if ~isempty(x)
-        plot(x,1,[cueID_color(goTrial) '-'  stateID_shape(1)],'MarkerFaceColor',cueID_color(goTrial))
+    xp=thisTrials(hit);
+    yl=licks(xp);
+    if ~isempty(xp)
+        subplot(2,1,1);
+        plot(xp,1,[cueID_color(goTrial) stateID_shape(1)],'MarkerFaceColor',cueID_color(goTrial))
+        subplot(2,1,2);
+        plot(xp,yl,[cueID_color(goTrial) stateID_shape(1)],'MarkerFaceColor',cueID_color(goTrial))
     end
-    x=thisTrials(miss);
-    if ~isempty(x)
-        plot(x,0,[cueID_color(goTrial) '-' stateID_shape(1)],'MarkerFaceColor',cueID_color(goTrial))
+
+    xp=thisTrials(miss);
+    yl=licks(xp);
+    if ~isempty(xp)
+        subplot(2,1,1);
+        plot(xp,0,[cueID_color(goTrial) stateID_shape(1)])
+        subplot(2,1,2);
+        plot(xp,yl,[cueID_color(goTrial) stateID_shape(1)])
     end
-    x=thisTrials(FA);
-    if ~isempty(x)
-        plot(x,1,[cueID_color(nogoTrial) '-' stateID_shape(2)],'MarkerFaceColor',cueID_color(nogoTrial))
+
+    xp=thisTrials(FA);
+    yl=licks(xp);
+    if ~isempty(xp)
+        subplot(2,1,1);
+        plot(xp,1,[cueID_color(nogoTrial) stateID_shape(2)])
+        subplot(2,1,2);
+        plot(xp,yl,[cueID_color(nogoTrial) stateID_shape(2)])
     end
-    x=thisTrials(CR);
-    if ~isempty(x)
-        plot(x,0,[cueID_color(nogoTrial) '-' stateID_shape(2)],'MarkerFaceColor',cueID_color(nogoTrial))
+
+    xp=thisTrials(CR);
+    yl=licks(xp);
+    if ~isempty(xp)
+        subplot(2,1,1);
+        plot(xp,0,[cueID_color(nogoTrial) stateID_shape(2)],'MarkerFaceColor',cueID_color(nogoTrial))
+        subplot(2,1,2);
+        plot(xp,yl,[cueID_color(nogoTrial) stateID_shape(2)],'MarkerFaceColor',cueID_color(nogoTrial))
     end
+
+    subplot(2,1,1);
     plot(thisTrials,1.1*ones(size(thisTrials)),['-' bloc_color(b)])
-    text(mean(thisTrials),1.4,sprintf('blocID %d', blocID(b)));
-    text(mean(thisTrials),1.2,sprintf('hit-FA : %.2f, d-prime : %.2f',hitRate(b)-FARate(b), dprime(b)));
+    text(thisTrials(5),1.4,sprintf('blocID %d', blocID(b)));
+    text(thisTrials(5),1.2,sprintf('hit-FA : %.2f, d-prime : %.2f',hitRate(b)-FARate(b), dprime(b)));
+    subplot(2,1,2);
 % Lick rate
-    subplot(2,1,2); hold on
-    xg=thisTrials(thisfGo);
-    yg=licks(xg);
-    plot(xg,yg,[cueID_color(goTrial) stateID_shape(1)],'MarkerFaceColor',cueID_color(goTrial))
-    xng=thisTrials(thisfNoGo);
-    yng=licks(xng);
-    plot(xng,yng,[cueID_color(nogoTrial) stateID_shape(2)],'MarkerFaceColor',cueID_color(nogoTrial))
-
+    subplot(2,1,2);
     plot(thisTrials,9*ones(size(thisTrials)),['-' bloc_color(b)])
-    text(thisTrials(5),10,sprintf('go : %.2f , nogo : %.2f (Hz)',mean(yg),mean(yng)));
-
+    text(thisTrials(5),10,sprintf('go : %.2f , nogo : %.2f (Hz)',lickRate(1),lickRate(1)));
+    catch
+    end
 end
 subplot(2,1,1);
-legend('Cue1','Cue2','Cue3','GoCue','NogoCue','Location','northwest')
+legend('Cue1','Cue2','Cue3','GoCue','NogoCue')
 xlim([0 length(tSequence)])
 ylim([-0.2 1.8]);
 yticks([0 1])
